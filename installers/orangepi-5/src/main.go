e986415a2048104ec8a6da30bbe10fa69b8588d2
Louis SCHNEIDER <louis@schne.id>
2024-03-17 21:14:36 +0100

    update orangepi-5 installer

--- installers/orangepi-5/src/main.go
+++ installers/orangepi-5/src/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
+	"os"
+	"path/filepath"
 
+	"github.com/siderolabs/go-copy/copy"
 	"github.com/siderolabs/talos/pkg/machinery/overlay"
 	"github.com/siderolabs/talos/pkg/machinery/overlay/adapter"
+	"golang.org/x/sys/unix"
+)
+
+const (
+	dtb = "rockchip/rk3588s-orangepi5.dtb"
 )
 
 func main() {
@@ -21,6 +30,7 @@ type boardExtraOptions struct {
 func (i *BoardInstaller) GetOptions(extra boardExtraOptions) (overlay.Options, error) {
 	kernelArgs := []string{
 		"console=tty0",
+		"console=ttyS2,1500000n8",
 		"sysctl.kernel.kexec_load_disabled=1",
 		"talos.dashboard.disabled=1",
 	}
@@ -34,5 +44,27 @@ func (i *BoardInstaller) GetOptions(extra boardExtraOptions) (overlay.Options, e
 }
 
 func (i *BoardInstaller) Install(options overlay.InstallOptions[boardExtraOptions]) error {
-	return nil
+	var f *os.File
+
+	f, err := os.OpenFile(options.InstallDisk, os.O_RDWR|unix.O_CLOEXEC, 0o666)
+	if err != nil {
+		return fmt.Errorf("failed to open %s: %w", options.InstallDisk, err)
+	}
+
+	defer f.Close() //nolint:errcheck
+
+	err = f.Sync()
+	if err != nil {
+		return err
+	}
+
+	src := filepath.Join(options.ArtifactsPath, "arm64/dtb", dtb)
+	dst := filepath.Join(options.MountPrefix, "/boot/EFI/dtb", dtb)
+
+	err = os.MkdirAll(filepath.Dir(dst), 0o600)
+	if err != nil {
+		return err
+	}
+
+	return copy.File(src, dst)
 }
